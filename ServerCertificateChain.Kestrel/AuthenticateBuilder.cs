47da9839cd96f0983f1d5d47542b38098a7d85e1
陈国伟 <366193849@qq.com>
2026-06-16 10:36:33 +0800

    完善注释

--- ServerCertificateChain.Kestrel/AuthenticateBuilder.cs
+++ ServerCertificateChain.Kestrel/AuthenticateBuilder.cs
@@ -5,12 +5,22 @@ using System.Net.Security;
 
 namespace ServerCertificateChain.Kestrel
 {
+    /// <summary>
+    /// ssl 认证构建器，负责构建一个认证处理器，该处理器会在 Kestrel 的 HTTPS 中间件中被调用，以配置 SSL 认证选项。
+    /// </summary>
     sealed class AuthenticateBuilder
     {
         private readonly Action<ConnectionContext, SslServerAuthenticationOptions> _fallbackHandler = (_, _) => { };
         private readonly List<Func<Action<ConnectionContext, SslServerAuthenticationOptions>, Action<ConnectionContext, SslServerAuthenticationOptions>>> _middlewares = [];
 
-        public void UseUserAuthenticate(Action<ConnectionContext, SslServerAuthenticationOptions>? useOnAuthenticate, Action<SslServerAuthenticationOptions> configureOptions)
+        /// <summary>
+        /// 使用用户的认证逻辑来配置 SSL 认证选项。该方法只能调用一次，并且必须在任何自定义认证中间件之前调用。
+        /// </summary>
+        /// <param name="useOnAuthenticate">用户认证逻辑</param>
+        /// <param name="configureOptions">额外配置</param>
+        public void UseUserAuthenticate(
+            Action<ConnectionContext, SslServerAuthenticationOptions>? useOnAuthenticate,
+            Action<SslServerAuthenticationOptions> configureOptions)
         {
             if (this._middlewares.Count == 0)
             {
@@ -23,6 +33,11 @@ namespace ServerCertificateChain.Kestrel
             }
         }
 
+        /// <summary>
+        /// 使用自定义的认证中间件来配置 SSL 认证选项
+        /// </summary>
+        /// <param name="middleware"></param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void UseCustomAuthenticate(Func<Action<ConnectionContext, SslServerAuthenticationOptions>, Action<ConnectionContext, SslServerAuthenticationOptions>> middleware)
         {
             if (this._middlewares.Count == 0)
@@ -35,6 +50,10 @@ namespace ServerCertificateChain.Kestrel
             this._middlewares.Insert(1, middleware);
         }
 
+        /// <summary>
+        /// 创建一个认证处理器，该处理器会在 Kestrel 的 HTTPS 中间件中被调用，以配置 SSL 认证选项。
+        /// </summary>
+        /// <returns></returns>
         public Action<ConnectionContext, SslServerAuthenticationOptions> Build()
         {
             var handler = this._fallbackHandler;
--- ServerCertificateChain.Kestrel/X509Certificate2Chain.cs
+++ ServerCertificateChain.Kestrel/X509Certificate2Chain.cs
@@ -9,16 +9,30 @@ using System.Text;
 
 namespace ServerCertificateChain.Kestrel
 {
+    /// <summary>
+    /// 表示一个服务器证书链，包含一个目标证书和零个或多个中间证书。
+    /// </summary>
     sealed class X509Certificate2Chain : X509Certificate2Collection
     {
         private const string PathKey = "Path";
         private const string PasswordKey = "Password";
         private const string CertificateLabel = "CERTIFICATE";
 
+        /// <summary>
+        /// 获取服务器证书链的目标（叶子）证书。
+        /// </summary>
         public X509Certificate2 TargetCertificate { get; }
 
+        /// <summary>
+        /// 获取服务器证书链的 SSL 证书上下文。该上下文会在第一次访问时创建，并且在整个生命周期内保持不变。
+        /// </summary>
         public Lazy<SslStreamCertificateContext> CertificateContext { get; }
 
+        /// <summary>
+        /// 服务器证书链
+        /// </summary>
+        /// <param name="targetCertificate">目标（叶子）证书</param>
+        /// <param name="intermediateCertificates">中间证书</param>
         public X509Certificate2Chain(
             X509Certificate2 targetCertificate,
             X509Certificate2Collection intermediateCertificates) : base(intermediateCertificates)
@@ -29,11 +43,22 @@ namespace ServerCertificateChain.Kestrel
 
         private SslStreamCertificateContext CreateCertificateContext()
         {
+            if (this.Count == 0)
+            {
+                SslStreamCertificateContext.Create(this.TargetCertificate, null);
+            }
+
             var certificateTrust = SslCertificateTrust.CreateForX509Collection(this);
-            return SslStreamCertificateContext.Create(this.TargetCertificate, null, false, certificateTrust);
+            return SslStreamCertificateContext.Create(this.TargetCertificate, this, false, certificateTrust);
         }
 
-        public static X509Certificate2Chain? ParseFromConfigSection(X509Certificate2 certificate, IConfigurationSection configurationSection)
+        /// <summary>
+        /// 从配置节中解析服务器证书链。
+        /// </summary>
+        /// <param name="targetCertificate">目标（叶子）证书</param>
+        /// <param name="configurationSection">证书的配置节</param>
+        /// <returns></returns>
+        public static X509Certificate2Chain? ParseFromConfigSection(X509Certificate2 targetCertificate, IConfigurationSection configurationSection)
         {
             var certPath = configurationSection.GetValue<string>(PathKey);
             if (certPath == null || File.Exists(certPath) == false)
@@ -48,7 +73,7 @@ namespace ServerCertificateChain.Kestrel
                 var collection = new X509Certificate2Collection();
                 // 不需要叶子证书，因此可以先加载到内存中，随后再释放。
                 collection.Import(certBytes, certPassword, X509KeyStorageFlags.EphemeralKeySet);
-                return CreateCertificate2Chain(certificate, collection);
+                return CreateCertificate2Chain(targetCertificate, collection);
             }
 
             var certPem = Encoding.UTF8.GetString(certBytes);
@@ -56,7 +81,7 @@ namespace ServerCertificateChain.Kestrel
             {
                 var collection = new X509Certificate2Collection();
                 collection.ImportFromPem(certPem);
-                return CreateCertificate2Chain(certificate, collection);
+                return CreateCertificate2Chain(targetCertificate, collection);
             }
 
             return null;
@@ -67,14 +92,14 @@ namespace ServerCertificateChain.Kestrel
             }
         }
 
-        private static X509Certificate2Chain? CreateCertificate2Chain(X509Certificate2 certificate, X509Certificate2Collection collection)
+        private static X509Certificate2Chain? CreateCertificate2Chain(X509Certificate2 targetCertificate, X509Certificate2Collection collection)
         {
-            var leafCert = collection.FirstOrDefault(i => i.Thumbprint == certificate.Thumbprint);
+            var leafCert = collection.FirstOrDefault(i => i.Thumbprint == targetCertificate.Thumbprint);
             if (leafCert != null)
             {
                 collection.Remove(leafCert);
                 leafCert.Dispose();
-                return new X509Certificate2Chain(certificate, collection);
+                return new X509Certificate2Chain(targetCertificate, collection);
             }
 
             foreach (var cert in collection)
